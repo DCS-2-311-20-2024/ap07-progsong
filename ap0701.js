5905d0c2a148b076a9dcc56391e615f949f035e5
progsong <228409@st.takushoku-u.ac.jp>
2024-11-13 15:05:27 +0900

    7回課題

--- ap0701.js
+++ ap0701.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第7回 課題1 (ap0701.js)
-// G38400-2023 拓殖太郎
+// G284092022 五十嵐健翔
 //
 "use strict"; // 厳格モード
 
@@ -14,11 +14,25 @@ import GUI from "ili-gui";
 function init() {
   // 制御変数の定義
   const param = {
-    axes: true, // 座標軸
+    opacity: 0.5, //透明度
+    background: false, //背景
+    birdsEye: false, //俯瞰
+    follow:false,//追跡
+    course:false, //コース
+    axes: false, // 座標軸
   };
 
   // GUIコントローラの設定
   const gui = new GUI();
+  gui.add(param,"opacity",0.0,1.0).name("ビルの透明度")
+    .onChange(()=>{
+      buildings.children.forEach((building)=>{
+        building.material.opacity=param.opacity;
+      })
+    });
+    gui.add(param,"background").name("背景");
+    gui.add(param,"follow").name("追跡");
+    gui.add(param,"birdsEye").name("俯瞰");
   gui.add(param, "axes").name("座標軸");
 
   // シーン作成
@@ -31,7 +45,7 @@ function init() {
   // カメラの作成
   const camera = new THREE.PerspectiveCamera(
     50, window.innerWidth/window.innerHeight, 0.1, 1000);
-  camera.position.set(6,2,2);
+  camera.position.set(150,80,0);//(6,2,2);
   camera.lookAt(0,0,0);
 
   // レンダラの設定
@@ -47,14 +61,33 @@ function init() {
   // モデルの読み込み
   let xwing; // モデルを格納する変数
   function loadModel() { // モデル読み込み関数の定義
-    render(); // 描画開始
+    const loader = new GLTFLoader();
+    loader.load(
+      "xwing.glb",//モデルのファイル
+      (gltf) =>{//読み込み終了時に実行される関数
+        xwing = gltf.scene;//モデルのシーンを取り出す
+        scene.add(xwing);
+        //render(); // 描画開始
+        setBackground();
+      }
+    );
   }
   loadModel(); // モデル読み込み実行
 
   // 背景の設定
   let renderTarget;
   function setBackground() {
-
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(
+      "csl01.jpg",
+      ()=>{
+        renderTarget
+         =new THREE.WebGLCubeRenderTarget(texture.image.height);
+         renderTarget.fromEquirectangularTexture(renderer,texture);
+         scene.background = renderTarget.texture;
+         render();
+      }
+    )
   }
 
   // 光源の設定
@@ -80,7 +113,24 @@ function init() {
     const d = 10;
     const gap = 10;
     const n = 6;
-
+    for(let c=0;c<n;c++){
+      for(let r=0;r<n;r++){
+        const building = new THREE.Mesh(
+          new THREE.BoxGeometry(w,h,d),
+          new THREE.MeshPhongMaterial({
+            color:0x408080,
+            opacity: param.opacity,
+            transparent: true
+          })
+        );
+        building.position.set(
+          (w+gap)*(r-(n-1)/2),
+          0,
+          (d+gap)*(c-(n-1)/2)
+        )
+        buildings.add(building);
+      }
+    }
   }
   scene.add(buildings);
 
@@ -106,9 +156,26 @@ function init() {
     [0, 0, -40],
   ]
   // コースの補間
-
+  const p0 = new THREE.Vector3();//p0,p1を用意
+  const p1 = new THREE.Vector3();
+  const course = new THREE.CatmullRomCurve3(
+    controlPoints.map((p,i)=>{
+      p0.set(...p);//制御点の配列からp0を取り出す
+      p1.set(...controlPoints[(i+1)%controlPoints.length]);//p0の次の点をp1に指定
+      return [
+        (new THREE.Vector3()).copy(p0),
+        (new THREE.Vector3()).lerpVectors(p0,p1,1/3),
+        (new THREE.Vector3()).lerpVectors(p0,p1,2/3),//p0とp1の間の1/3と2/3を計算
+      ];
+    }).flat(),true
+  );
   // コースの描画
-
+  const points = course.getPoints(300);
+  const courseObject = new THREE.Line(
+    new THREE.BufferGeometry().setFromPoints(points),
+    new THREE.LineBasicMaterial({color:"red"})
+  );
+  scene.add(courseObject);
   // Windowサイズの変更処理
   window.addEventListener("resize", ()=>{
     camera.aspect = window.innerWidth/window.innerHeight;
@@ -119,14 +186,41 @@ function init() {
   // 描画処理
   // 描画のための変数
   const clock = new THREE.Clock();
+  const xwingPosition = new THREE.Vector3();
+  const xwingTarget = new THREE.Vector3();
+  const cameraPosition = new THREE.Vector3();
   // 描画関数
   function render() {
     // xwing の位置と向きの設定
-
+    const elapsedTime = clock.getElapsedTime()/30;
+    course.getPointAt(elapsedTime%1,xwingPosition);
+    xwing.position.copy(xwingPosition);
+    course.getPointAt((elapsedTime+0.01)%1,xwingTarget);
+    xwing.lookAt(xwingTarget);
     // 背景の切り替え
- 
+    if(param.background){
+      scene.background = renderTarget.texture;
+      plane.visible = false;
+    }else{
+      scene.background = null;
+      plane.visible = true;
+    }
     // カメラの位置の切り替え
- 
+    if(param.follow){
+      cameraPosition.lerpVectors(xwingTarget,xwingPosition,4);
+      cameraPosition.y +=2.5;
+      camera.position.copy(cameraPosition);
+      camera.lookAt(xwing.position);
+      camera.up.set(0,1,0);
+    }else if(param.birdsEye){
+      camera.position.set(0,150,0);//上空から
+      camera.lookAt(plane.position);//平面の中央を見る
+      camera.up.set(0,1,0);//カメラの上をy軸正の向きにする
+    }else{
+      camera.position.set(10,-10,10);//下の方から
+      camera.lookAt(xwing.position);//飛行機を見る
+      camera.up.set(0,1,0)//カメラの上をy軸正の向きにする
+    }
     // コース表示の有無
     
     // 座標表示の有無
